cb9db68d8c99a372054cc8d767cef18f3d0e9432
ajiways <ajiwayswc@gmail.com>
2022-06-03 17:10:00 +0600

    #1 Edited common entities stuff

--- src/common/helpers/dto.ts
+++ src/common/helpers/dto.ts
@@ -1,5 +1,29 @@
 export type Complete<T> = {
-  [P in keyof Required<T>]: Pick<T, P> extends Required<Pick<T, P>>
-    ? T[P]
-    : T[P] | undefined;
+  [P in keyof Required<
+    Omit<
+      T,
+      | 'id'
+      | 'createdAt'
+      | 'deletedAt'
+      | 'deleterId'
+      | 'creatorId'
+      | 'editedAt'
+      | 'editorId'
+    >
+  >]: Pick<T, P> extends Required<Pick<T, P>> ? T[P] : T[P] | undefined;
 };
+
+export type CompleteToUpdate<T> = {
+  [P in keyof Partial<
+    Omit<
+      T,
+      | 'id'
+      | 'createdAt'
+      | 'deletedAt'
+      | 'deleterId'
+      | 'creatorId'
+      | 'editedAt'
+      | 'editorId'
+    >
+  >]: Pick<T, P> extends Partial<Pick<T, P>> ? T[P] : T[P] | undefined;
+} & { id: string };
