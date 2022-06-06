b298a3fe01feaa961d9747c9548e6309dfa9880b
ajiways <ajiwayswc@gmail.com>
2022-06-06 18:14:05 +0600

    #4 Schedule enum

---
+++ src/common/enums/schedule.enums.ts
@@ -0,0 +1,15 @@
+export enum ELessonType {
+  PZ = 'ПЗ',
+  LK = 'ЛК',
+  LB = 'ЛБ',
+}
+
+export enum EDayNames {
+  MONDAY = 'Понедельник',
+  TEUSDAY = 'Вторник',
+  WEDNESDAY = 'Среда',
+  THURSDAY = 'Четверг',
+  FRIDAY = 'Пятница',
+  SATURDAY = 'Суббота',
+  SUNDAY = 'Воскресенье',
+}
